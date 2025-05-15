d9859a7ddd6adab51569fccaf5b3f290a64285b6
yossivaturi <yossivaturi@gmail.com>
2025-05-15 12:31:27 +0300

    delete a task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,6 +12,14 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new Error(`Task with ID "${id}" not found`);
+    }
+    return found;
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
 
@@ -23,5 +31,11 @@ export class TasksService {
     };
     this.tasks.push(task);
     return task;
+  }
+
+  deleteTask(id: string): void {
+    // const found = this.getTaskById(id);
+    // this.tasks.splice(this.tasks.indexOf(found), 1);
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
