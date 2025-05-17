35ef3b14be7582261bd8b4bda2b60c4335f194cc
yossivaturi <yossivaturi@gmail.com>
2025-05-17 11:14:42 +0300

    added validation decorators for user input in GetTasksFilterDto

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  //added validation decorators for user input
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
